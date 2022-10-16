25e71031c9a19ea83a5d6bda91d393f1b2c57540
mkmdi <mkmdi@DESKTOP-N295UVR>
2022-10-16 14:37:04 +0300

    16.10.2

--- Domain/Entities/ProTechnology.cs
+++ Domain/Entities/ProTechnology.cs
@@ -4,6 +4,7 @@ namespace Domain.Entities
 {
     public class ProTechnology:Entity
     {
+        public int ProLangId { get; set; }
         public string Name { get; set; }
         public ProLang ProLang { get; set; }
     }
--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -14,14 +14,23 @@ namespace Persistence.Contexts
         }
 
         public DbSet<ProLang> ProLangs { get; set; }
-
+        public DbSet<ProTechnology> ProTechnologies{ get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ProLang>(p =>
             {
-                p.ToTable("ProLang").HasKey(k => k.Id);
+                p.ToTable("ProLangs").HasKey(k => k.Id);
                 p.Property(k => k.Id).HasColumnName("Id");
                 p.Property(k => k.Name).HasColumnName("Name");
+                p.HasIndex(k => k.Name).IsUnique();
+            });
+            modelBuilder.Entity<ProTechnology>(p =>
+            {
+                p.ToTable("ProTechnologies").HasKey(k => k.Id);
+                p.Property(k => k.Id).HasColumnName("Id");
+                p.Property(k => k.Name).HasColumnName("Name");
+                p.HasIndex(k => k.Name).IsUnique();
+                p.HasOne(k => k.ProLang).WithMany(k => k.ProTechnologies).HasForeignKey(k => k.ProLangId);
             });
         }
     }
